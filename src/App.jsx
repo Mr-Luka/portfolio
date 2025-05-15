83929db3a8f38512f9fcdd7fc240b4b5ae202416
Luka <luka.luka.luka93@gmail.com>
2025-05-15 14:02:37 -0700

    added background video, changed css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import NavBar from './components/NavBar.jsx';
 import HomePage from "./components/Home-page.jsx";
+import About from './components/About.jsx';
+import Portfolio from './components/Portfolio.jsx';
 
+import backgroundVideo from './assets/background.mp4'
 import './main.css';
 
 function App() {
@@ -9,9 +12,15 @@ function App() {
   return (
    <>
     <section className="home">
+    <video src={backgroundVideo} autoPlay loop muted></video>
         <NavBar />
         <HomePage />
     </section>
+    <hr></hr>
+    <div className='sections'>
+      <About />
+      <Portfolio />
+    </div>
    </>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+export default function About(){
+
+    return (
+        <>
+            <div className='about'>
+                <h4>About me:</h4>
+                <p>skdjskd kjsjdskldjs ksjdkslj dkljdkljskd jksjks djksd ksjdksjdksdjk jkjdsd jskdjskd </p>
+            </div>
+        </>
+    )
+}
